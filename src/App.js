1de6abd3c3735ac372e0519d5b079c6c8398f578
ishtiaque786 <ishtiaque.rakib@northsouth.edu>
2022-03-22 14:54:10 +0600

    installed firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import initializeFirebase from "./Firebase/firebase.init";
 
+initializeFirebase();
 function App() {
   return (
     <div className="App">
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0jEQatpyjO7zhFXGyJUUe4B_caSGSDd4",
+  authDomain: "my-app-536e2.firebaseapp.com",
+  projectId: "my-app-536e2",
+  storageBucket: "my-app-536e2.appspot.com",
+  messagingSenderId: "668175278657",
+  appId: "1:668175278657:web:c383ce0a2f3f33304bdcc0",
+};
+
+export default firebaseConfig;
